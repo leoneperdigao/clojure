878df5e055da01349ed81267fdce32e165aed766
Leone Perdigão <leone.lage@gmail.com>
2019-02-02 21:24:51 -0200

    Initial commit

---
+++ hello-wold/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hello-wold
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ hello-wold/test/hello_wold/core_test.clj
@@ -0,0 +1,7 @@
+(ns hello-wold.core-test
+  (:require [clojure.test :refer :all]
+            [hello-wold.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
